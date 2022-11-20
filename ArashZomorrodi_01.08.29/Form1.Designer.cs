bb5313b7c15f56222cace89b649b45231964155d
4R4S8 <arashzomorrodi24@gmail.com>
2022-11-20 23:21:19 +0330

    Final

---
+++ ArashZomorrodi_01.08.29/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace ArashZomorrodi_01._08._29
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.InputTxt = new System.Windows.Forms.RichTextBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RefTxt2nd = new System.Windows.Forms.TextBox();
+            this.ResMetRef = new System.Windows.Forms.Label();
+            this.RefTxt = new System.Windows.Forms.TextBox();
+            this.MetRefStartBtn = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Txt2nd = new System.Windows.Forms.TextBox();
+            this.ResMet = new System.Windows.Forms.Label();
+            this.Txt = new System.Windows.Forms.TextBox();
+            this.MetStartBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.InputTxt);
+            this.groupBox1.Controls.Add(this.StartBtn);
+            this.groupBox1.Location = new System.Drawing.Point(18, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(318, 544);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Space Remover (w/ Replace)";
+            // 
+            // InputTxt
+            // 
+            this.InputTxt.Font = new System.Drawing.Font("Persian", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputTxt.Location = new System.Drawing.Point(28, 59);
+            this.InputTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.InputTxt.Name = "InputTxt";
+            this.InputTxt.Size = new System.Drawing.Size(253, 271);
+            this.InputTxt.TabIndex = 1;
+            this.InputTxt.Text = "";
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(76, 388);
+            this.StartBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(155, 104);
+            this.StartBtn.TabIndex = 0;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.RefTxt2nd);
+            this.groupBox2.Controls.Add(this.ResMetRef);
+            this.groupBox2.Controls.Add(this.RefTxt);
+            this.groupBox2.Controls.Add(this.MetRefStartBtn);
+            this.groupBox2.Location = new System.Drawing.Point(339, 15);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(226, 543);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Methodes (w/ ref)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 151);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "+";
+            // 
+            // RefTxt2nd
+            // 
+            this.RefTxt2nd.Font = new System.Drawing.Font("Persian", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RefTxt2nd.Location = new System.Drawing.Point(58, 196);
+            this.RefTxt2nd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RefTxt2nd.Multiline = true;
+            this.RefTxt2nd.Name = "RefTxt2nd";
+            this.RefTxt2nd.Size = new System.Drawing.Size(100, 63);
+            this.RefTxt2nd.TabIndex = 3;
+            this.RefTxt2nd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ResMetRef
+            // 
+            this.ResMetRef.AutoSize = true;
+            this.ResMetRef.Location = new System.Drawing.Point(54, 308);
+            this.ResMetRef.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ResMetRef.Name = "ResMetRef";
+            this.ResMetRef.Size = new System.Drawing.Size(72, 23);
+            this.ResMetRef.TabIndex = 2;
+            this.ResMetRef.Text = "Result :";
+            // 
+            // RefTxt
+            // 
+            this.RefTxt.Font = new System.Drawing.Font("Persian", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RefTxt.Location = new System.Drawing.Point(58, 58);
+            this.RefTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RefTxt.Multiline = true;
+            this.RefTxt.Name = "RefTxt";
+            this.RefTxt.Size = new System.Drawing.Size(100, 63);
+            this.RefTxt.TabIndex = 1;
+            this.RefTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MetRefStartBtn
+            // 
+            this.MetRefStartBtn.Location = new System.Drawing.Point(58, 387);
+            this.MetRefStartBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MetRefStartBtn.Name = "MetRefStartBtn";
+            this.MetRefStartBtn.Size = new System.Drawing.Size(101, 104);
+            this.MetRefStartBtn.TabIndex = 0;
+            this.MetRefStartBtn.Text = "Start";
+            this.MetRefStartBtn.UseVisualStyleBackColor = true;
+            this.MetRefStartBtn.Click += new System.EventHandler(this.MetRefStartBtn_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.Txt2nd);
+            this.groupBox3.Controls.Add(this.ResMet);
+            this.groupBox3.Controls.Add(this.Txt);
+            this.groupBox3.Controls.Add(this.MetStartBtn);
+            this.groupBox3.Location = new System.Drawing.Point(572, 19);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(226, 543);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Methodes (w/o ref)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 150);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "+";
+            // 
+            // Txt2nd
+            // 
+            this.Txt2nd.Font = new System.Drawing.Font("Persian", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt2nd.Location = new System.Drawing.Point(58, 194);
+            this.Txt2nd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt2nd.Multiline = true;
+            this.Txt2nd.Name = "Txt2nd";
+            this.Txt2nd.Size = new System.Drawing.Size(100, 63);
+            this.Txt2nd.TabIndex = 3;
+            this.Txt2nd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ResMet
+            // 
+            this.ResMet.AutoSize = true;
+            this.ResMet.Location = new System.Drawing.Point(54, 307);
+            this.ResMet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ResMet.Name = "ResMet";
+            this.ResMet.Size = new System.Drawing.Size(72, 23);
+            this.ResMet.TabIndex = 2;
+            this.ResMet.Text = "Result :";
+            // 
+            // Txt
+            // 
+            this.Txt.Font = new System.Drawing.Font("Persian", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt.Location = new System.Drawing.Point(58, 58);
+            this.Txt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt.Multiline = true;
+            this.Txt.Name = "Txt";
+            this.Txt.Size = new System.Drawing.Size(100, 63);
+            this.Txt.TabIndex = 1;
+            this.Txt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MetStartBtn
+            // 
+            this.MetStartBtn.Location = new System.Drawing.Point(58, 387);
+            this.MetStartBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MetStartBtn.Name = "MetStartBtn";
+            this.MetStartBtn.Size = new System.Drawing.Size(101, 104);
+            this.MetStartBtn.TabIndex = 0;
+            this.MetStartBtn.Text = "Start";
+            this.MetStartBtn.UseVisualStyleBackColor = true;
+            this.MetStartBtn.Click += new System.EventHandler(this.MetStartBtn_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 578);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Persian", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox InputTxt;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label ResMetRef;
+        private System.Windows.Forms.TextBox RefTxt;
+        private System.Windows.Forms.Button MetRefStartBtn;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label ResMet;
+        private System.Windows.Forms.TextBox Txt;
+        private System.Windows.Forms.Button MetStartBtn;
+        private System.Windows.Forms.TextBox RefTxt2nd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txt2nd;
+    }
+}
+
---
+++ ArashZomorrodi_01.08.29/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArashZomorrodi_01._08._29
+{
+    public partial class Home : Form
+    {
+        static int number;
+        public Home()
+        {
+            InitializeComponent();
+        }
+         void RefMet(int var)
+        {
+            number = Convert.ToInt32(RefTxt2nd.Text);
+            var += number; 
+            
+        }
+
+        void Met(ref int var)
+        {
+            number = Convert.ToInt32(RefTxt2nd.Text);
+            var += number;
+
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (InputTxt.Text!="")
+            {
+                string Result = InputTxt.Text.Replace(" ", "");
+                if (Result!=InputTxt.Text)
+                {
+                    DialogResult res = new DialogResult();
+                    res = MessageBox.Show("\"" + Result + "\" :شکل نهایی رشته\n آیا می‌خواهید که متن نهایی روی متن وارد شده اعمال شود؟", "شکل نهاییِ رشته", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    switch (res)
+                    {
+                        case DialogResult.Yes:
+                            {
+                                InputTxt.Text = Result;
+                                MessageBox.Show("تغییرات اعمال شد", "پیغام", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            break;
+                        case DialogResult.No:
+                            {
+                                InputTxt.Focus();
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else if (Result==InputTxt.Text)
+                {
+
+                    MessageBox.Show("!مقدار ورودی شما فاقد فاصله است", "پیغام", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    
+                }
+                else
+                {
+                    MessageBox.Show("لطفاً بعداً دوباره تلاش کنید", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            else if(InputTxt.Text == "")
+            {
+                MessageBox.Show("!ورودی نباید خالی باشد", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                InputTxt.Focus();
+            }
+            else
+            {
+                MessageBox.Show("لطفاً بعداً دوباره تلاش کنید", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void MetRefStartBtn_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(RefTxt.Text);
+            
+            RefMet(num);
+            ResMetRef.Text = "Result: " + num + "";
+        }
+
+        private void MetStartBtn_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(Txt.Text);
+
+            Met(ref num);
+            ResMet.Text = "Result: " + num + "";
+
+
+        }
+
+        
+    }
+}
